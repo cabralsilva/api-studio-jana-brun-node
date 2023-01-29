b18931923b574c9c3e7c77aafbce206886a922ba
Daniel <d.nadson@yahoo.com.br>
2023-01-29 20:11:17 -0300

    fix quantity e items os matriculation

--- dist/src/model/schema/SkuItem.js
+++ dist/src/model/schema/SkuItem.js
@@ -5,7 +5,7 @@ const mongoose = require("mongoose");
 const SkuItemModel = {
     product: { type: mongoose.Schema.Types.ObjectId, ref: 'product', required: true },
     grateItemList: [{ type: mongoose.Schema.Types.ObjectId, ref: 'grateItem', required: true }],
-    quantityValue: { type: Number, required: true, default: 1 },
+    quantity: { type: Number, required: true, default: 1 },
     unitValue: { type: Number, required: true },
     totalValue: { type: Number, required: true }
 };
