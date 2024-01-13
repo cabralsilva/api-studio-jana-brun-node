a551631370c7edc05bf48eaf75e5090a4a591b11
DanielDeuPetch <daniel@deupetch.com.br>
2024-01-13 11:54:11 -0300

    feat: adicionando sequencial na venda 2

--- dist/src/flow/sale/item/GetSequenceFlowItem.js
+++ dist/src/flow/sale/item/GetSequenceFlowItem.js
@@ -12,20 +12,25 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const IMatriculation_1 = require("../../../model/schema/IMatriculation");
+const c2_mongoose_1 = require("c2-mongoose");
 const ISale_1 = require("../../../model/schema/ISale");
 const StringUtils_1 = __importDefault(require("../../../utils/StringUtils"));
 class GetSequenceFlowItem {
+    constructor() {
+        this.searcherSale = new c2_mongoose_1.CrudFlow(ISale_1.SaleRepository);
+    }
     get(offset = 0) {
+        var _a;
         return __awaiter(this, void 0, void 0, function* () {
             var sequence = StringUtils_1.default.padToLeft("0", 6, 1);
-            var count = yield new ISale_1.SaleSearch({
-                orderBy: "sequence _id",
-                order: "desc",
-                page: 1,
-                limit: 1
-            }).count(IMatriculation_1.MatriculationRepository);
-            sequence = StringUtils_1.default.padToLeft("0", 6, (Number(count) + 1 + offset));
+            this.searcherSale.prepareSearch(new ISale_1.SaleSearch({
+                orderBy: "sequence",
+                orderSense: "desc",
+                limit: 1,
+            }));
+            const search = yield this.searcherSale.find({});
+            const last = search.items[0];
+            sequence = StringUtils_1.default.padToLeft("0", 6, (Number((_a = last === null || last === void 0 ? void 0 : last.sequence) !== null && _a !== void 0 ? _a : 0) + 1 + offset));
             return sequence;
         });
     }
--- src/flow/sale/item/GetSequenceFlowItem.ts
+++ src/flow/sale/item/GetSequenceFlowItem.ts
@@ -1,20 +1,23 @@
+import { CrudFlow } from "c2-mongoose";
 import { MatriculationRepository, MatriculationSearchOLD } from "../../../model/schema/IMatriculation";
-import { SaleSearch } from "../../../model/schema/ISale";
+import { ISale, SaleRepository, SaleSearch } from "../../../model/schema/ISale";
 import StringUtils from "../../../utils/StringUtils";
 
 class GetSequenceFlowItem {
 
+  private searcherSale = new CrudFlow<ISale>(SaleRepository)
+
   async get(offset: number = 0) {
     var sequence = StringUtils.padToLeft("0", 6, 1)
     
-    var count = await new SaleSearch({
-        orderBy: "sequence _id",
-        order: "desc",
-        page: 1,
-        limit: 1
-      }).count(MatriculationRepository)
-    
-    sequence = StringUtils.padToLeft("0", 6, (Number(count) + 1 + offset))
+    this.searcherSale.prepareSearch(new SaleSearch({
+      orderBy: "sequence",
+      orderSense: "desc",
+      limit: 1,
+    }))
+    const search = await this.searcherSale.find({})
+    const last = search.items[0]
+    sequence = StringUtils.padToLeft("0", 6, (Number(last?.sequence ?? 0) + 1 + offset))
     
     return sequence
   }
